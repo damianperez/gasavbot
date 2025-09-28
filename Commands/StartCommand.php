d4b4b8fa2412e5be1c86b9dbe643411bc999ea31
damianperez <perezdamian@gmail.com>
2025-09-28 11:15:41 -0300

    VSc

--- Commands/StartCommand.php
+++ Commands/StartCommand.php
@@ -11,36 +11,37 @@
  */
 
 /**
- * User "/Start" command
+ * Start command
  *
- * Simple command that returns info about the current user.
+ * Gets executed when a user first starts using the bot.
+ *
+ * When using deep-linking, the parameter can be accessed by getting the command text.
+ *
+ * @see https://core.telegram.org/bots#deep-linking
  */
 
-namespace Longman\TelegramBot\Commands\UserCommands;
+namespace Longman\TelegramBot\Commands\SystemCommands;
 
-use Longman\TelegramBot\ChatAction;
-use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
-use Longman\TelegramBot\Entities\UserProfilePhotos;
 use Longman\TelegramBot\Exception\TelegramException;
-use Longman\TelegramBot\Request;
 
-class StartCommand extends UserCommand
+class StartCommand extends SystemCommand
 {
     /**
      * @var string
      */
-    protected $name = 'Start';
+    protected $name = 'start';
 
     /**
      * @var string
      */
-    protected $description = 'Show your id, name and username';
+    protected $description = 'Start command';
 
     /**
      * @var string
      */
-    protected $usage = '/Start';
+    protected $usage = '/start';
 
     /**
      * @var string
@@ -60,66 +61,12 @@ class StartCommand extends UserCommand
      */
     public function execute(): ServerResponse
     {
-        $message = $this->getMessage();
+        // If you use deep-linking, get the parameter like this:
+        // $deep_linking_parameter = $this->getMessage()->getText(true);
 
-        $from       = $message->getFrom();
-        $user_id    = $from->getId();
-        $chat_id    = $message->getChat()->getId();
-        $message_id = $message->getMessageId();
-
-        $data = [
-            'chat_id'             => $chat_id,
-            'reply_to_message_id' => $message_id,
-        ];
-
-        // Send chat action "typing..."
-        Request::sendChatAction([
-            'chat_id' => $chat_id,
-            'action'  => ChatAction::TYPING,
-        ]);
-
-        $caption = sprintf(
-            'Your Id: %d' . PHP_EOL .
-            'Name: %s %s' . PHP_EOL .
-            'Username: %s',
-            $user_id,
-            $from->getFirstName(),
-            $from->getLastName(),
-            $from->getUsername()
+        return $this->replyToChat(
+            'Hi there!' . PHP_EOL .
+            'Type /help to see all commands!'
         );
-        $caption = "Bienvenido a Gasav ".$from->getLastName()." ($user_id ".  $from->getUsername().")";
-
-        // Fetch the most recent user profile photo
-        $limit  = 1;
-        $offset = null;
-
-        $user_profile_photos_response = Request::getUserProfilePhotos([
-            'user_id' => $user_id,
-            'limit'   => $limit,
-            'offset'  => $offset,
-        ]);
-
-        if ($user_profile_photos_response->isOk()) {
-            /** @var UserProfilePhotos $user_profile_photos */
-            $user_profile_photos = $user_profile_photos_response->getResult();
-
-            if ($user_profile_photos->getTotalCount() > 0) {
-                $photos = $user_profile_photos->getPhotos();
-
-                // Get the best quality of the profile photo
-                $photo   = end($photos[0]);
-                $file_id = $photo->getFileId();
-
-                $data['photo']   = $file_id;
-                $data['caption'] = $caption;
-
-                return Request::sendPhoto($data);
-            }
-        }
-
-        // No Photo just send text
-        $data['text'] = $caption;
-
-        return Request::sendMessage($data);
     }
 }
--- CustomCommands/StartCommand.php
+++ CustomCommands/StartCommand.php
@@ -11,37 +11,36 @@
  */
 
 /**
- * Start command
+ * User "/Start" command
  *
- * Gets executed when a user first starts using the bot.
- *
- * When using deep-linking, the parameter can be accessed by getting the command text.
- *
- * @see https://core.telegram.org/bots#deep-linking
+ * Simple command that returns info about the current user.
  */
 
-namespace Longman\TelegramBot\Commands\SystemCommands;
+namespace Longman\TelegramBot\Commands\UserCommands;
 
-use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\ChatAction;
+use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Entities\UserProfilePhotos;
 use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
 
-class StartCommand extends SystemCommand
+class StartCommand extends UserCommand
 {
     /**
      * @var string
      */
-    protected $name = 'start';
+    protected $name = 'Start';
 
     /**
      * @var string
      */
-    protected $description = 'Start command';
+    protected $description = 'Show your id, name and username';
 
     /**
      * @var string
      */
-    protected $usage = '/start';
+    protected $usage = '/Start';
 
     /**
      * @var string
@@ -61,12 +60,66 @@ class StartCommand extends SystemCommand
      */
     public function execute(): ServerResponse
     {
-        // If you use deep-linking, get the parameter like this:
-        // $deep_linking_parameter = $this->getMessage()->getText(true);
+        $message = $this->getMessage();
 
-        return $this->replyToChat(
-            'Hola!' . PHP_EOL .
-            'Type /help to see all commands!'
+        $from       = $message->getFrom();
+        $user_id    = $from->getId();
+        $chat_id    = $message->getChat()->getId();
+        $message_id = $message->getMessageId();
+
+        $data = [
+            'chat_id'             => $chat_id,
+            'reply_to_message_id' => $message_id,
+        ];
+
+        // Send chat action "typing..."
+        Request::sendChatAction([
+            'chat_id' => $chat_id,
+            'action'  => ChatAction::TYPING,
+        ]);
+
+        $caption = sprintf(
+            'Your Id: %d' . PHP_EOL .
+            'Name: %s %s' . PHP_EOL .
+            'Username: %s',
+            $user_id,
+            $from->getFirstName(),
+            $from->getLastName(),
+            $from->getUsername()
         );
+        $caption = "Bienvenido a Gasav ".$from->getLastName()." ($user_id ".  $from->getUsername().")";
+
+        // Fetch the most recent user profile photo
+        $limit  = 1;
+        $offset = null;
+
+        $user_profile_photos_response = Request::getUserProfilePhotos([
+            'user_id' => $user_id,
+            'limit'   => $limit,
+            'offset'  => $offset,
+        ]);
+
+        if ($user_profile_photos_response->isOk()) {
+            /** @var UserProfilePhotos $user_profile_photos */
+            $user_profile_photos = $user_profile_photos_response->getResult();
+
+            if ($user_profile_photos->getTotalCount() > 0) {
+                $photos = $user_profile_photos->getPhotos();
+
+                // Get the best quality of the profile photo
+                $photo   = end($photos[0]);
+                $file_id = $photo->getFileId();
+
+                $data['photo']   = $file_id;
+                $data['caption'] = $caption;
+
+                return Request::sendPhoto($data);
+            }
+        }
+
+        // No Photo just send text
+        $data['text'] = $caption;
+
+        return Request::sendMessage($data);
     }
 }
