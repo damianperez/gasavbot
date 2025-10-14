b48d5602b2fadcf32f35314bc1ab179482ff4dc5
Damian Perez <dperez@maa.gba.gov.ar>
2025-10-14 10:39:38 -0300

    Foto

--- CustomCommands/varios/FotoCommand.php
+++ CustomCommands/varios/FotoCommand.php
@@ -75,6 +75,45 @@ class FotoCommand extends UserCommand
         $message = $this->getMessage();
         $text    = $message->getText(true);
 
+        $message = $this->getMessage();
+        $from       = $message->getFrom();
+        $user_id    = $from->getId();
+        $chat_id    = $message->getChat()->getId();
+        $message_id = $message->getMessageId();
+
+        $data = [
+            'chat_id'             => $chat_id,
+ //           'reply_to_message_id' => $message_id,
+            'parse_mode' => 'HTML',
+        ];
+
+
+        $ip_address = '200.114.85.10'; // Replace with your camera's IP
+        $port = '80'; // Replace with your camera's HTTP port if different
+        $channel = '0'; // Adjust if needed for specific channel
+        $username = 'Ver'; // Replace with your camera username
+        $password = 'PlayaGasav'; // Replace with your camera password
+
+        $snapshot_url = "http://{$username}:{$password}@{$ip_address}:{$port}/cgi-bin/snapshot.cgi?channel={$channel}";
+
+        // Retrieve the image data
+        //$image_data = @file_get_contents($snapshot_url);
+
+        $data['caption'] = 'foto';
+        $data['photo']   = Request::encodeFile($snapshot_url);	        
+        Request::sendPhoto($data);     
+
+
+
+
+        if ($image_data !== false) {
+            // Set the appropriate header for image output
+            header('Content-Type: image/jpeg');
+            echo $image_data;
+        } else {
+            echo "Error: Could not retrieve snapshot from Dahua camera.";
+        }
+
         $texto='aa';
         return $this->replyToChat($texto ,['parse_mode' => 'HTML',]);
     }
