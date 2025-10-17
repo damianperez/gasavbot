7856e37c465bb7a00368c704e80499800a422ffa
Damian Perez <dperez@maa.gba.gov.ar>
2025-10-17 13:13:16 -0300

    norden api

--- api/Norden.php
+++ api/Norden.php
@@ -122,7 +122,7 @@ $opciones =[];
         $key = $keys[$i];
         $value = (float) $data[$key]['T'];        
         if ($value > $alto) { $ialto=$i; $alto = $value; }
-        if ($value < $bajo) $ibajo=$i; $bajo = $value; }
+        if ($value < $bajo) { $ibajo=$i; $bajo = $value; }
         echo "Key: $key, Index: $i  Value: $value $alto $bajo \n";
     }
     $primer_valor = $data[array_key_first($data)]['T'];
