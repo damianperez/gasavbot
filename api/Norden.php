a573110c5c9a9d9b8a9da5181ea6f8713a73420e
Damian Perez <dperez@maa.gba.gov.ar>
2025-10-22 12:33:49 -0300

    norden api

--- api/Norden.php
+++ api/Norden.php
@@ -67,7 +67,7 @@ $opciones =[];
     $latest_estacion = $valores[5]->nodeValue;
 
 
-    $LECTURAS = 120;
+    $LECTURAS = -120;
     $wind = (array) $data->wind->chart->gust->series{1}->data;
     $uwind = array_reverse(array_slice( $wind, $LECTURAS  ));
     $tide = (array) $data->tide->chart->series;
